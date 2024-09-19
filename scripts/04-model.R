15564f06fae599d48f61615e0b8b47411fac5671
cZhuang17 <chiyue.zhuang@mail.utoronto.ca>
2024-09-19 18:06:24 -0400

    Update 04-model.R

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models
+# Author: Chiyue Zhuang
+# Date: 19 September 2024
+# Contact: chiyue.zhuang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
