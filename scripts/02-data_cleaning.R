70c9ba8ec9674956a4b75a8e64d45e0464f60c78
cZhuang17 <chiyue.zhuang@mail.utoronto.ca>
2024-09-19 18:01:47 -0400

    Update 02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Cleans the raw marriage data into an analysis dataset
-# Author: Rohan Alexander
+# Author: Chiyue Zhuang
 # Date: 19 September 2024
-# Contact: rohan.alexander@utoronto.ca
+# Contact: chiyue.zhuang@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Need to have downloaded the data
 # Any other information needed? None.
